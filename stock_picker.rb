67da6174bd22b1c5d2419ab1c3af41d784ff3fda
Altervl <62514019+Altervl@users.noreply.github.com>
2024-10-02 20:49:50 +0300

    Do minor improvements to stock_picker

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,24 +1,24 @@
 def stock_picker(prices)
   return "not enough data" if prices.size <= 1
   
-  min_price_day = 0
+  min_price = prices[0]
   buy_day = 0
   sell_day = 1
   max_profit = prices[sell_day] - prices[buy_day]
 
   prices.each_with_index do |price, day|
     # Check for new min price
-    if price < prices[min_price_day]
-      min_price_day = day
+    if price < min_price
+      min_price = price
     end
 
     # Calculate profit for current day
-    profit = price - prices[min_price_day]
+    profit = price - min_price
 
     # Update max_profit and buy, sell days if current day profit is better
     if profit > max_profit
       max_profit = profit
-      buy_day = min_price_day
+      buy_day = prices.index(min_price)
       sell_day = day
     end
   end
