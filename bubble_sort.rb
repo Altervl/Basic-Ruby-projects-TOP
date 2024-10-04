46180fdfee01b55c8f2277b01d488225472a64b2
Altervl <62514019+Altervl@users.noreply.github.com>
2024-10-04 12:47:04 +0300

    Make small optimization
    Skip check of the last checked item

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,27 @@
 def bubble_sort(array)
-# flag for swapping places
+  # flag for swapping places
   swap = true
+  
+  # save array size value for later use
+  n = array.size
 
-# run the loop while replacements in array occur
+  # run the loop while replacements in array occur
   while swap
     # set the flag to false on every iteration
     swap = false
+
     # run through the array until main loop stops, skip the last item
-    (array.size - 1).times do |i|
-    # compare adjacent items, swap their places if they're out of order
-    # set the flag to true
+    (n - 1).times do |i|
+      # compare adjacent items, swap their places if they're out of order
+      # set the flag to true
       if array[i] > array[i + 1]
         array[i], array[i + 1] = array[i + 1], array[i]
         swap = true
       end
     end
+    # optimization: skip the last checked item on next iteration
+    # because it's already in place
+    n -= 1
   end
 
   # when main loop ends return sorted array
